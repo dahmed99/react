ee5b22c215d36c7bd390122a0cc2143a62dde8b9
Daniel Ahmed <daniel@daniel-ahmed.com>
2023-10-11 10:15:19 +0100

    Initial Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ObservabilityApp from './ObservabilityApp';
+
+function App() {
+  return (
+    <div className="App">
+      <ObservabilityApp />
+    </div>
+  );
+}
+
+export default App;
