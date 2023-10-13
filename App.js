d5a8323f5b81ad788a2b88be9db34ba57d3d7c88
Daniel Ahmed <daniel@daniel-ahmed.com>
2023-10-13 09:39:47 +0100

    App.js

--- App.js
+++ App.js
@@ -1,14 +1,80 @@
-// src/App.js
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ObservabilityApp from './ObservabilityApp';
+import React, { useState } from 'react';
+import { Container, Navbar, Form, Button } from 'react-bootstrap';
 
 function App() {
+  const [formData, setFormData] = useState({
+    platform: '',
+    repoUrl: '',
+    branch: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://test.com/1', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        console.log('Form data sent successfully!');
+      } else {
+        console.error('Failed to send form data.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <ObservabilityApp />
-    </div>
+    <Container>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>PTSRE Observability Automation</Navbar.Brand>
+      </Navbar>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Platform</Form.Label>
+          <Form.Control
+            type="text"
+            name="platform"
+            value={formData.platform}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Repo URL</Form.Label>
+          <Form.Control
+            type="text"
+            name="repoUrl"
+            value={formData.repoUrl}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Branch</Form.Label>
+          <Form.Control
+            type="text"
+            name="branch"
+            value={formData.branch}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
