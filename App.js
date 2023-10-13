c2d55863aa9c9c0e4debeedaf66b5d71995956ba
Daniel Ahmed <daniel@daniel-ahmed.com>
2023-10-13 10:28:20 +0100

    lol

--- App.js
+++ App.js
@@ -1,57 +1,39 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  function submitHandler(e) {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-
-    const data = {
-      input1: formData.get('input1'),
-      // Repeat for input2-5
-    };
-
-    fetch('https://test.com/test', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data); // Handle the response as needed
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-
   return (
-    <div className="App">
-      <header>
-        <nav className="navbar">
-          <div className="logo">Your Logo</div>
-          <ul className="nav-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <form onSubmit={submitHandler}>
-          <div className="form-group">
-            <label htmlFor="input1">Input 1:</label>
-            <input type="text" id="input1" name="input1" />
-          </div>
+    <div className="container d-flex justify-content-center align-items-center h-100">
+      <form>
+        <div className="form-group">
+          <label htmlFor="input1">Input 1:</label>
+          <input type="text" className="form-control" id="input1" name="input1" />
+        </div>
 
-          {/* Repeat this for inputs 2-5 */}
-          
-          <button type="submit">Submit</button>
-        </form>
-      </main>
+        {/* Repeat this for inputs 2-5 */}
+        <div className="form-group">
+          <label htmlFor="input2">Input 2:</label>
+          <input type="text" className="form-control" id="input2" name="input2" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="input3">Input 3:</label>
+          <input type="text" className="form-control" id="input3" name="input3" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="input4">Input 4:</label>
+          <input type="text" className="form-control" id="input4" name="input4" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="input5">Input 5:</label>
+          <input type="text" className="form-control" id="input5" name="input5" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
