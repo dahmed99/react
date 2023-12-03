f2e549c2d339f5341079e528cae2f35c5a491203
Daniel Ahmed <daniel@daniel-ahmed.com>
2023-12-03 09:43:33 +0000

    aed

--- src/FormTest.js
+++ src/FormTest.js
@@ -1,24 +1,40 @@
 // FormPage.test.js
+
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import FormPage from './FormPage'; // Import your FormPage component
+import '@testing-library/jest-dom/extend-expect'; // For additional matchers
 
-test('Form submits with correct values', () => {
-  // Mock any necessary functions or data if needed
+import FormPage from './FormPage'; // Import your actual FormPage component
 
-  render(<FormPage />); // Render your FormPage component
+describe('FormPage', () => {
+  it('should handle form submission', () => {
+    render(<FormPage />); // Render your FormPage component
 
-  // Simulate user input
-  fireEvent.change(screen.getByTestId('input_file_path'), { target: { value: 'file/path' } });
-  fireEvent.change(screen.getByTestId('platform'), { target: { value: 'somePlatform' } });
-  fireEvent.change(screen.getByTestId('repo_clone_url'), { target: { value: 'https://github.com/example/repo.git' } });
-  fireEvent.change(screen.getByTestId('branch'), { target: { value: 'main' } });
-  fireEvent.change(screen.getByTestId('eks_terraform_repo'), { target: { value: 'https://github.com/example/eks-terraform.git' } });
-  fireEvent.change(screen.getByTestId('eks_terraform_branch'), { target: { value: 'master' } });
+    // Mock data for testing
+    const formData = {
+      input_file_path: 'example_path',
+      platform: 'example_platform',
+      repo_clone_url: 'https://github.com/example/repo.git',
+      branch: 'main',
+      eks_terraform_repo: 'https://github.com/example/eks-terraform.git',
+      eks_terraform_branch: 'master',
+    };
 
-  // Trigger form submission
-  fireEvent.submit(screen.getByTestId('your-form-id')); // Replace 'your-form-id' with the actual ID of your form
+    // Iterate over the formData and populate the corresponding input fields
+    Object.entries(formData).forEach(([key, value]) => {
+      const inputElement = screen.getByTestId(`input_${key}`);
+      fireEvent.change(inputElement, { target: { value } });
+    });
 
-  // Add assertions based on your form submission logic
-  // For example, you can check if a callback function is called, if a loading state is set, etc.
+    // Trigger form submission
+    const submitButton = screen.getByText('Submit'); // Adjust this based on your actual submit button text
+    fireEvent.click(submitButton);
+
+    // Add assertions based on your expected behavior after form submission
+    // For example, you might check for the presence of a success message
+
+    // For demonstration purposes, let's check if a success message is present
+    const successMessage = screen.getByTestId('success-message');
+    expect(successMessage).toBeInTheDocument();
+  });
 });
